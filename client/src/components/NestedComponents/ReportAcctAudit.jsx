369aef1d65976cdaa2e10b0a344d3d0ee9e8f799
Sieh Johnson <johnson_sieh@yahoo.com>
2018-10-31 18:56:22 -0500

    Create Account Audit component

---
+++ client/src/components/NestedComponents/ReportAcctAudit.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AccountAuditTool extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        ACCOUNT AUDIT TOOL
+      </div>
+    )
+  }
+}
+
+export default AccountAuditTool;

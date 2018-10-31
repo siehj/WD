e6139779e4ba5c299e73107a587c34468d60f389
Sieh Johnson <johnson_sieh@yahoo.com>
2018-10-31 18:23:56 -0500

    Fix syntax error in requiring axios

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import axios from 'axios';
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();

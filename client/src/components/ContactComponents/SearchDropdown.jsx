e2e14368062acd7b522eeb9d2af42968a67c016e
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-02 17:54:18 -0500

    Add folder for contact components

---
+++ client/src/components/ContactComponents/SearchDropdown.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { DropdownMenu, DropdownItem } from 'reactstrap';
+
+const SearchDropdown = () => {
+  return (
+    <DropdownMenu>
+      <option></option>
+      {/* <option>Another Action</option>
+      <option>Another Action</option> */}
+    </DropdownMenu>
+  )
+}
+
+export default SearchDropdown;

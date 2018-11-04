a2fad3c3ce0f37ae1668d1d7cbd63b3d119236aa
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-03 23:22:19 -0500

    Create a contact modal skeleton for future use

---
+++ client/src/components/ContactComponents/ContactModal.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { 
+  Button, Modal, 
+  ModalHeader, ModalBody, 
+  ModalFooter, Input, InputGroupAddon, InputGroupText,
+  Label, Form, InputGroup } from 'reactstrap';
+
+const ContactModal = (props) => {
+  return (
+    <div>
+      <Modal isOpen={props.status} >
+        <ModalHeader className="text-center"> Create A New Contact </ModalHeader>
+        <ModalBody>
+          <Form>
+            <InputGroup>
+            <InputGroupAddon addonType="prepend" >
+              <InputGroupText>Contact</InputGroupText>
+            </InputGroupAddon>
+              <Input/>
+            </InputGroup>
+            <InputGroup>
+            <InputGroupAddon addonType="prepend" >
+              <InputGroupText>Note</InputGroupText>
+            </InputGroupAddon>
+              <Input/>
+            </InputGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button size="sm" title="Contact" color="success" >Create</Button>
+          <Button size="sm" title="Contact" onClick={props.toggleContactModal} >Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
+}
+
+export default ContactModal;

e9606780bb990ffb363bb1340c4d1ae3bd1e79ef
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-03 23:21:55 -0500

    Reformat schema to be imported into Hosted DB

--- schema.sql
+++ schema.sql
@@ -1,48 +1,58 @@
-CREATE DATABASE WDdatabase;
+-- CREATE DATABASE WDdatabase;
 
-USE WDdatabase;
+USE WDdb;
 
-CREATE TABLE contacts (
-  id int NOT NULL AUTO_INCREMENT,
-  Name varchar(100), 
-  Company varchar(100),
-  Phone varchar(12),
-  Email varchar(100)
-  PRIMARY KEY (ID)
-);
-
-CREATE TABLE companyType (
-  id int NOT NULL AUTO_INCREMENT,
-  Name text,
-  PRIMARY KEY (ID)
-)
-ALTER TABLE 'contacts' ADD FOREIGN KEY (type_id) REFERENCES 'companyType' ('id');
-
-CREATE TABLE employees (
-  id int NOT NULL AUTO_INCREMENT,
-  Name varchar(100), 
-  Username varchar(100),
-  Password varchar(100),
-  Position int,
-  Admin BOOLEAN,
-  PRIMARY KEY (ID)
-)
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companyType;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS positions;
 
 CREATE TABLE positions (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   position varchar(100)
-  PRIMARY KEY (ID)
-)
+);
+
+
+CREATE TABLE employees (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(100), 
+  username varchar(100),
+  password varchar(100),
+  admin_status BOOLEAN,
+  position_id int,
+  FOREIGN KEY (position_id) REFERENCES positions(id)
+);
+
 
 CREATE TABLE tasks (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   task text, 
   note varchar(200), 
-  completed Boolean,
+  completed BOOLEAN,
   created datetime,
   deadline datetime,
   priority int,
-  PRIMARY KEY (ID)
-)
+  employee_id int,
+  FOREIGN KEY (employee_id) REFERENCES employees(id)
+);
 
-ALTER TABLE 'tasks' ADD FOREIGN KEY (employee_id) REFERENCES 'employees' ('id');
+
+CREATE TABLE companyType (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Name text
+);
+
+
+CREATE TABLE contacts (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(100), 
+  company varchar(100),
+  phone varchar(12),
+  email varchar(100),
+  type_id int,
+  FOREIGN KEY (type_id) REFERENCES companyType(id)
+);
+
+
+

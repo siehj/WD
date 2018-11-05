b820bc8320d0cc6d9562f55483a6155e4e057624
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-05 12:06:23 -0600

    Finish routes for all task/db communication

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const http = require('http');
 const socketIo = require('socket.io');
 const server = http.createServer(app);
 const io = socketIo(server);
-// const db = require('../Database');
+// import { fakeData } from './fakeData.js';
+const db = require('../Database');
 // const fs = require('fs');
 
 

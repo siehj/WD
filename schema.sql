2dbe4fa69404ea32354cda55c4b9e6e60ab62591
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-07 18:41:38 -0600

    Add pool table in schema for unassigned and completed tasks

--- schema.sql
+++ schema.sql
@@ -53,5 +53,17 @@ CREATE TABLE contacts (
   FOREIGN KEY (type_id) REFERENCES companyType(id)
 );
 
+CREATE TABLE pool (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  task text, 
+  note varchar(200), 
+  completed BOOLEAN,
+  created datetime,
+  deadline datetime,
+  priority int,
+  employee_id int,
+  FOREIGN KEY (employee_id) REFERENCES employees(id)
+);
+
 
 

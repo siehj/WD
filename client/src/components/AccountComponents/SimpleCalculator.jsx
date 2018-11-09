27566dd578863578b7985329c5557de64339564e
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-08 22:16:02 -0600

    Implement auto calculation of credits/balances

--- client/src/components/AccountComponents/SimpleCalculator.jsx
+++ client/src/components/AccountComponents/SimpleCalculator.jsx
@@ -5,17 +5,16 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      col: {
-        "Office Fee": 0,
-        Allowed: 0,
-        "Writeoff": 0,
-        Primary: 0,
-        PP: 0,
-        Paid: 0,
-        Total: 0
-      },
+      col: [ "Office Fee", "Allowed", "Writeoff", "Primary", "PP", "Paid", "Total" ],
       SecIns: 0,
       NewTotal: 0,
+      OF: 0.00, 
+      Allowed: 0, 
+      WO: 0, 
+      Primary: 0, 
+      PP: 0, 
+      Paid: 0,
+      Total: 0,
       secondary: false
     };
     this.calculateWriteOff = this.calculateWriteOff.bind(this);
@@ -26,20 +25,35 @@ class Calculator extends React.Component {
 
   calculateWriteOff() {
     // Take Office Fee - Allowed Amount = Writeoff
+    let OfficeFee = this.state.OF;
+    let Allowed = this.state.Allowed;
+    let WriteOff = OfficeFee - Allowed;
+    this.setState({ WO : WriteOff }, () => this.calculateTotal());
   }
 
   calculateTotal() {
-    if (!this.state.secondary) {
-      // 
-
-    } else {
-      // must calculate with the secondary numbers 
-    }
+    // allowed - primary = pp
+    let Allowed = this.state.Allowed;
+    let Primary = this.state.Primary;
+    let PP = Allowed - Primary;
+    
+    this.setState({ PP : PP }, () => {
+      // pp - paid = total
+      let Paid = this.state.Paid;
+      let Calc = this.state.PP - Paid;
+      this.setState({ Total : Calc }, () => {
+        if (this.state.secondary) {
+          let preTotal = this.state.Total;
+          let Sec = this.state.SecIns;
+          let NewCalc = preTotal - Sec;
+          this.setState({ NewTotal: NewCalc });
+        }
+      });
+    });
   }
   
   changeValue(e) {
-    console.log(e.target.name);
-    console.log(e.target.value);
+    this.setState({ [`${e.target.name}`] : e.target.value }, () => this.calculateWriteOff());
   }
 
   addSecondary() {
@@ -53,27 +67,25 @@ class Calculator extends React.Component {
           <thead>
             <tr>
               {
-                Object.keys(this.state.col).map((col, i) => {
-                  return (
-                    <th key={i} >{col}</th>
-                    )
-                  })
-                }
+                this.state.col.map((col, i) => {
+                  return <th key={i}>{col}</th>
+                })
+              }
               { this.state.secondary ? <th>Secondary</th> : null}
               { this.state.secondary ? <th>New Total</th> : null}
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td> <Input type="number" name="Office Fee" placeholder="0.00" onChange={this.changeValue}/> </td>
+              <td> <Input type="number" name="OF" placeholder="0.00" onChange={this.changeValue}/> </td>
               <td> <Input type="number" name="Allowed" placeholder="0.00" onChange={this.changeValue}/> </td>
-              <td name="Write-off"></td>
+              <td name="WO" className="text-center"><h6>{this.state.WO.toFixed(2)}</h6></td>
               <td> <Input type="number" name="Primary" placeholder="0.00" onChange={this.changeValue}/></td>
-              <td> <Input type="number" name="PP" placeholder="0.00" onChange={this.changeValue}/></td>
+              <td name="PP" className="text-center"> <h6>{this.state.PP.toFixed(2)}</h6></td>
               <td> <Input type="number" name="Paid" placeholder="0.00" onChange={this.changeValue}/></td>
-              <td name="Total"></td>
+              <td name="Total" className="text-center"><h6>{this.state.Total.toFixed(2)}</h6></td>
               { this.state.secondary ? <td><Input type="number" name="SecIns" placeholder="0.00" onChange={this.changeValue}/></td> : null}
-              { this.state.secondary ? <td name="NewTotal" >0.00</td> : null}
+              { this.state.secondary ? <td name="NewTotal"className="text-center"><h6>{this.state.NewTotal.toFixed(2)}</h6></td> : null}
             </tr>
           </tbody>
         </Table>

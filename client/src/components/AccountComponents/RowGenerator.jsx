c0e4a52add3013072ec516ef12acc34138334ab2
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-09 02:06:29 -0600

    Begin implementation of a way to tally the total for the full audit

--- client/src/components/AccountComponents/RowGenerator.jsx
+++ client/src/components/AccountComponents/RowGenerator.jsx
@@ -3,6 +3,8 @@ import AcctRow from './AcctRow.jsx';
 
 const RowGenerator = (props) => {
  const rows = [];
+ const rowTots = {};
+
  for(let i = 0; i < props.num; i++) {
    rows.push(<AcctRow key={i} name={`row${i}`} sec={props.sec} off={props.off} />)
  }

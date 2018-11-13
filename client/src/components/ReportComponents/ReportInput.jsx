4133e4f508e8a4b8d7577e3fb14af1a5d307853e
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-13 14:44:28 -0600

    Create sideNav component for future refactor

--- client/src/components/ReportComponents/ReportInput.jsx
+++ client/src/components/ReportComponents/ReportInput.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Row, Col, Form, FormGroup, CustomInput } from 'reactstrap';
 
 const ReportInput = (props) => {
   return (
-    <div className="container" >
-    
-      
+    <div>
+      <Row>
+        <Col>
+          <Form>
+            <FormGroup>
+              <CustomInput type="file" id="FileBrowser" name="customFile" />
+            </FormGroup>
+          </Form>
+        </Col>
+      </Row>
+     
+      <div className="text-center" >
+         <p>Show and choose the sheet to save it to.</p>
+      </div>
+      {/* <div className="text-center" >
+        <p>Show new accounts to be audited</p>
+      </div> */}
     </div>
   )
 }
---
+++ client/src/components/SideNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Container, Row, Col, Button  } from 'reactstrap';
+
+
+const SideNav = (props) => {
+return <div></div>
+}
+
+export default SideNav;

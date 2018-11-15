abd55cb181edb0e83e303af8bc29ded3b6da8598
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-15 10:30:53 -0600

    Iplement the skeleton of a floating calculator for the app

---
+++ client/src/components/AdditionalComponents/DraggableCalc.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Row, Col, Button } from 'reactstrap';
+import Draggable from 'react-draggable';
+
+class DraggableCalc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      output: ''
+    };
+  }
+
+  render() {
+    return (
+      <Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{x: 30, y: 0}}
+        position={null}
+        grid={[25, 25]}
+        bounds="parent"
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}>
+        <div id="floatingCalc" >
+          <div style={{ backgroundColor: 'gray' }}> 
+            <div className="handle">Drag from here</div>
+          </div>
+          <div id="fcBody">
+            <input type="text" style={{ width: '100%', height: '42px' }} placeholder="0.00" />
+              <div className="calcBtnRow" >
+                <button>1</button><button>2</button><button>3</button><button>+</button>
+                <button>4</button><button>5</button><button>6</button><button>-</button>
+                <button>7</button><button>8</button><button>9</button><button>*</button>
+                <button>0</button><button>+/-</button><button>%</button><button>/</button>
+              </div>
+              <div className="lastRow" >
+                <button >clear</button>
+                <button >undo</button>
+              </div>
+              <div style={{ backgroundColor: '#fff' }} >
+                <Button outline color="success" block > = </Button>
+              </div>
+            {/* <div>This readme is really dragging on...</div> */}
+          </div>
+        </div>
+      </Draggable>
+    )
+  }
+}
+
+export default DraggableCalc;
